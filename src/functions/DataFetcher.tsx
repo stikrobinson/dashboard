a37894730e24323c13b3846a2876bf4ccb8e6143
stikrobinson <148508110+stikrobinson@users.noreply.github.com>
2025-07-11 00:31:20 +0000

    actividad en clases

--- src/functions/DataFetcher.tsx
+++ src/functions/DataFetcher.tsx
@@ -32,29 +32,7 @@ export default function DataFetcher(cityInput: string) : DataFetcherOutput {
         // Reemplace con su URL de la API de Open-Meteo obtenida en actividades previas
         const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m,wind_speed_10m&current=temperature_2m,relative_humidity_2m,wind_speed_10m,apparent_temperature&timezone=America%2FChicago&forecast_days=1`
         const fetchData = async () => {
-
-            try {
-
-                const response = await fetch(url);
-
-                if (!response.ok) {
-                    throw new Error(`Error HTTP: ${response.status} - ${response.statusText}`);
-                }
-
-                const result: OpenMeteoResponse = await response.json();
-                setData(result);
-
-            } catch (err: any) {
-
-                if (err instanceof Error) {
-                    setError(err.message);
-                } else {
-                    setError("Ocurrió un error desconocido al obtener los datos.");
-                }
-
-            } finally {
-                setLoading(false);
-            }
+            
         };
 
         fetchData();
@@ -62,5 +40,5 @@ export default function DataFetcher(cityInput: string) : DataFetcherOutput {
     }, [cityInput]); // El array vacío asegura que el efecto se ejecute solo una vez después del primer renderizado
 
     return { data, loading, error };
-
+     t
 }
